635137208f4c100f8115e3b11e38b5aa281ccee9
m2wasabi <m2wasabi@gmail.com>
2018-04-16 07:43:15 +0900

    Test: Change API URL

--- tests/Feature/Route/PsersonTest.php
+++ tests/Feature/Route/PsersonTest.php
@@ -20,32 +20,32 @@ class PsersonTest extends TestCase
 
     public function testPersonList()
     {
-        $response = $this->json('GET','person');
+        $response = $this->json('GET','api/person');
         $response->assertStatus(200);
     }
 
     public function testPersonStore()
     {
-        $response = $this->json('POST','person',
+        $response = $this->json('POST','api/person',
             ['name' => 'Johnny', 'weight' => '60', 'height' => '1.72']);
         $response->assertStatus(200);
     }
 
     public function testPersonView()
     {
-        $response = $this->json('GET','person/1');
+        $response = $this->json('GET','api/person/1');
         $response->assertStatus(200);
     }
 
     public function testPersonUpdate()
     {
-        $response = $this->json('PUT','person/1', ['name' => 'Johnson']);
+        $response = $this->json('PUT','api/person/1', ['name' => 'Johnson']);
         $response->assertStatus(200);
     }
 
     public function testPersonDelete()
     {
-        $response = $this->json('DELETE','person/1');
+        $response = $this->json('DELETE','api/person/1');
         $response->assertStatus(200);
     }
 
