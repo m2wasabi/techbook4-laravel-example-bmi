10f2dbff2ea79fb23d7b38960af28230d4d57fa3
m2wasabi <m2wasabi@gmail.com>
2018-04-16 04:35:48 +0900

    test/Route/PersonTest URL check

--- tests/Feature/Route/PsersonTest.php
+++ tests/Feature/Route/PsersonTest.php
@@ -17,4 +17,36 @@ class PsersonTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    public function testPersonList()
+    {
+        $response = $this->json('GET','person');
+        $response->assertStatus(200);
+    }
+
+    public function testPersonStore()
+    {
+        $response = $this->json('POST','person',
+            ['name' => 'Johnny', 'weight' => '60', 'height' => '1.72']);
+        $response->assertStatus(200);
+    }
+
+    public function testPersonView()
+    {
+        $response = $this->json('GET','person/1');
+        $response->assertStatus(200);
+    }
+
+    public function testPersonUpdate()
+    {
+        $response = $this->json('PUT','person/1', ['name' => 'Johnson']);
+        $response->assertStatus(200);
+    }
+
+    public function testPersonDelete()
+    {
+        $response = $this->json('DELETE','person/1');
+        $response->assertStatus(200);
+    }
+
 }
