b597b7b0ac46a2e0af2d5f44efc1bb2ce2eceb4a
m2wasabi <m2wasabi@gmail.com>
2018-04-16 21:53:37 +0900

    Create app/Services/BmiService

---
+++ app/Services/BmiService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+
+use App\Models\Person;
+
+class BmiService
+{
+
+    public static function getBmi(Person $person)
+    {
+
+    }
+
+    private static function calcBmi(float $height, float $weight)
+    {
+
+    }
+}
